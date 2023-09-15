30f0997dbd3a35074b5b1f1ef4cc2386ce3ab453
ViniciusMdeiros <Vinicius-medeiros2011@hotmail.com>
2023-09-15 20:24:03 -0300

    Finishing course

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Balta.ContentContext;
+using Balta.SubscriptionContext;
 
 var articles = new List<Article>();
 articles.Add(new Article("Artigo sobre OOP", "orientacao-objetos"));
@@ -24,9 +25,9 @@ courses.Add(courseAspNet);
 
 var careers = new List<Career>();
 var careerDotNet = new Career("Especialista .NET", "Especialista-dotnet");
-var careerItem2 = new CareerItem(2, "Aprenda OOP", "", courseOOP);
+var careerItem2 = new CareerItem(2, "Aprenda OOP", "", null);
 var careerItem = new CareerItem(1, "Comece por aqui", "", courseCsharp);
-var careerItem3 = new CareerItem(3, "Aprenda .NET", "", couserAspNet);
+var careerItem3 = new CareerItem(3, "Aprenda .NET", "", courseAspNet);
 careerDotNet.Items.Add(careerItem2);
 careerDotNet.Items.Add(careerItem);
 careerDotNet.Items.Add(careerItem3);
@@ -38,7 +39,16 @@ foreach (var career in careers)
     foreach (var item in career.Items.OrderBy(x => x.Order))
     {
         Console.WriteLine($"{item.Order} - {item.Title}");
-        Console.WriteLine(item.Course.Title);
-        Console.WriteLine(item.Course.Level);
+        Console.WriteLine(item.Course?.Title);
+        Console.WriteLine(item.Course?.Level);
+
+        foreach (var notification in item.Notifications)
+        {
+            Console.WriteLine($"{notification.Property} - {notification.Message}");
+        }
     }
+
+    var payPalSubscription = new PayPalSubscription();
+    var student = new Student();
+    student.CreateSubscription(payPalSubscription);
 }
